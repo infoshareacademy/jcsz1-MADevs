6ee0cd63df670268ec2b752ec4d494678519035b
witkowiakm <witkowiak.m@gmail.com>
2019-04-19 23:30:12 +0200

    Added Database Model, settings in Startup.cs. Instaled EntityFrameworkCore.SQL, EntityFrameworkCore.Tools, EntityFrameworkCore.Design

---
+++ Common/Models/DataBaseModel.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Common.Models
+{
+    public class DataBaseModel
+    {
+        
+        public int id { get; set; }
+        public string place { get; set; }
+        public DateTime endDate { get; set; }
+        public string name { get; set; }
+        public string urls { get; set; }
+        public List<object> attachments { get; set; }
+        public string descLong { get; set; }
+        public int categoryId { get; set; }
+        public DateTime startDate { get; set; }
+        public string organizer { get; set; }
+        public int active { get; set; }
+        public string tickets { get; set; }
+        public string descShort { get; set; }
+        public string recurrence { get; set; }
+        public string schedule { get; set; }
+    }
+
+    public class EventsBase : IEntityTypeConfiguration<DataBaseModel>
+    {
+        public void Configure(EntityTypeBuilder<DataBaseModel> builder)
+        {
+            builder
+                .ToTable("Event")
+                .Property(x => x.id)
+                .IsRequired()
+                .HasColumnName("Id");
+
+            builder
+                .Property(x => x.place)
+                .HasMaxLength(50)
+                .HasColumnName("Place");
+            builder
+                .Property(x => x.endDate)
+                .HasColumnName("EndData");
+
+            builder
+                .Property(x => x.name)
+                .HasMaxLength(100)
+                .HasColumnName("Name");
+
+            builder
+                .Property(x => x.urls)
+                .HasColumnName("Url");
+
+            builder
+                .Property(x => x.attachments)
+                .HasColumnName("Attachments");
+
+            builder
+                .Property(x => x.descLong)
+                .HasMaxLength(1000)
+                .HasColumnName("DescLong");
+
+            builder
+                .Property(x => x.categoryId)
+                .HasColumnName("CategoryId");
+
+            builder
+                .Property(x => x.startDate)
+                .HasColumnName("startData");
+
+            builder
+                .Property(x => x.organizer)
+                .HasMaxLength(50)
+                .HasColumnName("Organizer");
+
+            builder
+                .Property(x => x.active)
+                .HasColumnName("Active");
+
+            builder
+                .Property(x => x.tickets)
+                .HasColumnName("Tickets");
+
+            builder
+                .Property(x => x.descShort)
+                .HasColumnName("DescShort");
+
+            builder
+                .Property(x => x.recurrence)
+                .HasColumnName("Recurrence");
+
+            builder
+                .Property(x => x.schedule)
+                .HasColumnName("Schedule");
+
+        }
+    }
+}
--- WebCultureInGdansk/Startup.cs
+++ WebCultureInGdansk/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace WebCultureInGdansk
 {
@@ -34,6 +36,9 @@ namespace WebCultureInGdansk
 
             services.AddSingleton<IEventsFromJson, EventsFromJson>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            var connection = @"Server=(localdb)\mssqllocaldb;Database=EFGetStarted.AspNetCore.NewDb;Trusted_Connection=True;ConnectRetryCount=0";
+            services.AddDbContext<Common.Models.EventsBase>
+                (options => options.UseSqlServer(connection));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
